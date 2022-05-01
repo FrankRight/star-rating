de79d48d6b0b2236eff8485de51d32a9c0e5388c
Frank Bosire <frankbosire2017@gmail.com>
2022-05-01 09:28:45 +0300

    updated react scripts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
+import { FaStar } from "react-icons/fa";
 
 function App() {
   return (
     <>
       <h1>Recipe App</h1>
+
+      <FaStar color="red" />
+      <FaStar color="red" />
+      <FaStar color="red" />
+      <FaStar color="grey" />
+      <FaStar color="grey" />
     </>
   );
 }
